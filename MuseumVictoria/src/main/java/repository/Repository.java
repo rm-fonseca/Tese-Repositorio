90eeabfb89c2f03f5db3ab126985d5e6ad2ef5a7
Rúben <rubenribeirogarcia@gmail.com>
2018-09-08 23:54:57 +0100

    Finished mapping part of Articles Model from Museum Victoria. At this moment it only mapps for articles an does not map all attributes. Next development is to finish mapping articles, an then pass to the other two record types

--- MuseumVictoria/src/main/java/repository/Repository.java
+++ MuseumVictoria/src/main/java/repository/Repository.java
@@ -8,7 +8,9 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.http.client.HttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.LaxRedirectStrategy;
@@ -21,13 +23,14 @@ import plataforma.modelointerno.LanguageString;
 import plataforma.modelointerno.Resource;
 import plataforma.modelointerno.Result;
 import repository.models.MuseumVictoriaArticleModel;
+import repository.models.MuseumVictoriaRecordTypeModel;
 
 public class Repository implements RepositoryAbstract {
 
 	private static final String QUERY_BY_TERM = "https://collections.museumvictoria.com.au/api/search?query=";
 	private static final String SOURCE_PAGE_BASE_URL = "https://collections.museumvictoria.com.au/"; //Get id attribute from json to concatnate
 	private static final String SOURCE_DATA_BASE_URL = "https://collections.museumvictoria.com.au/api/"; //Get id attribute from json to concatnate
-	private String repName = null;
+	private static String repName = null;
 
 	private static List<String> valuesDateEarly;
 	private static List<String> valuesDateLate;
@@ -46,143 +49,91 @@ public class Repository implements RepositoryAbstract {
 		final HttpClient httpClient = HttpClientBuilder.create().setRedirectStrategy(new LaxRedirectStrategy()).build();
 		factory.setHttpClient(httpClient);
 		restTemplate.setRequestFactory(factory);
+
+		// Convert to Results
+		List<Result> resultsList = new ArrayList<Result>();
+
 		// Request
 		String response = restTemplate.getForObject(QUERY_BY_TERM + term, String.class);
 		ObjectMapper mapper = new ObjectMapper();
-		MuseumVictoriaArticleModel[] article = mapper.readValue(response, MuseumVictoriaArticleModel[].class);
+		final ObjectNode[] nodes = mapper.readValue(response, ObjectNode[].class);
+		//MuseumVictoriaRecordTypeModel[] article = mapper.readValue(response, MuseumVictoriaRecordTypeModel[].class);
 
-		List<Result> resultsList = new ArrayList<Result>();
-		//JSONObject responseJson = new JSONObject(response);
-		JSONArray items = new JSONArray(response);
-		//JSONArray items = responseJson.getJSONArray("results");
-
-		JSONObject item;
-		JSONArray keyValues;
-
-		for (Object itemObject : items) {
-
-			valuesDateEarly = null;
-			valuesDateLate = null;
-
-			item = (JSONObject) itemObject;
-			Result result = new Result();
-			result.getSourcePage().add(SOURCE_PAGE_BASE_URL + item.getString("id"));
-			result.getSourceData().add(SOURCE_DATA_BASE_URL + item.getString("id"));
-			result.getSourceRepositorie().add(getRepositoryName());
-
-			Iterator<String> keys = item.keys();
-
-			String key;
-
-			while (keys.hasNext()) {
-				key = keys.next();
-
-				List<String> values = new ArrayList<String>();
-				keyValues = item.optJSONArray(key);
-
-				if (keyValues != null)
-					for (int j = 0; j < keyValues.length(); j++)
-						values.add(keyValues.get(j).toString());
-				else
-					values.add(item.get(key).toString());
-
-				connectData(key, values, result, ignoreExtraProperties);
-
+		for(ObjectNode node : nodes) {
+			if(node.has("recordType")) {
+				String recordType = node.get("recordType").asText().toUpperCase();
+				switch (recordType) {
+					case MuseumVictoriaRecordTypeModel.ARTICLE_RECORD_TYPE:
+						MuseumVictoriaArticleModel articleModel = mapper.readValue(node.toString(), MuseumVictoriaArticleModel.class);
+						connectDataArticle(articleModel, resultsList, ignoreExtraProperties);
+					default:
+						break;
+				}
 			}
-
-			resultsList.add(result);
-
 		}
 
 		return resultsList;
 	}
 
-	private static void connectData(String key, List<String> values, Result result, boolean ignoreExtraProperties) {
+	private static void connectDataArticle(MuseumVictoriaArticleModel articleModel, List<Result> results, boolean ignoreExtraProperties) throws IOException {
+		Result result = new Result();
+		result.getSourcePage().add(SOURCE_PAGE_BASE_URL + articleModel.id);
+		result.getSourceData().add(SOURCE_DATA_BASE_URL + articleModel.id);
+		result.getSourceRepositorie().add(getRepositoryName());
 
-		String newKey;
-		JSONArray keyValues;
-		LanguageString lang;
-		switch (key) {
-			case "recordType":
-				lang = getLanguageStringDef(result.getDcType());
-				lang.getText().addAll(values);
-				break;
-			case "licence":
-				//This Json attribute has 3 field
-				//name
-				//shortname
-				//
-				break;
-			case "dateModified":
-				break;
-			case "title":
-				break;
-			case "displayTitle":
-				break;
-			case "keywords":
-				break;
-			case "localities":
-				break;
-			case "content":
-				break;
-			case "contentSummary":
-				break;
-			case "types":
-				break;
-			case "authors":
-				break;
-			case "contributors":
-				break;
-			case "media":
-				break;
-			case "yearWritten":
-				break;
-			default:
-				if (ignoreExtraProperties)
-					break;
-				ExtraProperty properties = new ExtraProperty();
-				properties.setName(key);
-				properties.getValue().addAll(values);
-
-				result.getExtraProperties().add(properties);
+		//Record Type
+		result.getDcType().add(new LanguageString() {{
+			setLanguage("en");
+			getText().add(articleModel.recordType); }});
+		//License
+		result.getDcRights().add(new LanguageString() {{
+			setLanguage("en");
+			getText().add(articleModel.licence.name);}});
+		result.getDcRights().add(new LanguageString() {{
+			setLanguage("en");
+			getText().add(articleModel.licence.uri);}});
+		//Date Modified
+		result.getTermsModified().add(new LanguageString(){{
+			setLanguage("en");
+			getText().add(articleModel.dateModified);
+		}});
+		//Title & Display Title
+		result.getDcTitle().add(new LanguageString(){{
+			setLanguage("en");
+			getText().add(articleModel.title);
+			getText().add(articleModel.displayTitle);
+		}});
+		//Keywords
+		for(String keyword : articleModel.keywords) {
+			ExtraProperty extraProperty = new ExtraProperty();
+			extraProperty.setName("Keyword");
+			extraProperty.getValue().add(keyword);
+			result.getExtraProperties().add(extraProperty);
 		}
+		//Content & ContentSummary
+		result.getExtraProperties().add(new ExtraProperty(){{
+			setName("Content");
+			getValue().add(articleModel.content);
+		}});
+		result.getExtraProperties().add(new ExtraProperty(){{
+			setName("Content Summary");
+			getValue().add(articleModel.contentSummary);
+		}});
+		//Types
+		for(String type : articleModel.types) {
+			result.getDcType().add(new LanguageString(){{
+				setLanguage("en");
+				getText().add(type);
+			}});
+		}
+		results.add(result);
 	}
 
-	private static void connectDataImages(String key, List<String> values, Result result,
-										  boolean ignoreExtraProperties) {
+	private static void connectDataItem(Object itemModel, List<Result> results, boolean ignoreExtraProperties) {
 
-		switch (key) {
+	}
 
-			case "licenceURI":
-				LanguageString lang = new LanguageString();
-
-				lang = getLanguageStringDef(result.getDcRights());
-				lang.getText().addAll(values);
-				break;
-
-			case "thumbnailURI":
-
-				for (String value : values) {
-
-
-					Resource res = new Resource();
-					res.setType("Image");
-					res.setUrl(value);
-					result.getResources().add(res);
-				}
-
-				break;
-
-			default:
-				if (ignoreExtraProperties)
-					break;
-
-				ExtraProperty properties = new ExtraProperty();
-				properties.setName("images_" + key);
-				properties.getValue().addAll(values);
-
-				result.getExtraProperties().add(properties);
-		}
+	private static void connectDataSpeciment(Object specimenModel, List<Result> results, boolean ignoreExtraProperties) {
 
 	}
 
@@ -197,10 +148,8 @@ public class Repository implements RepositoryAbstract {
 		return lang;
 	}
 
-	private String getRepositoryName() throws IOException {
-
+	private static String getRepositoryName() throws IOException {
 		if (repName == null) {
-
 			Properties prop = new Properties();
 			InputStream input = null;
 
@@ -214,19 +163,14 @@ public class Repository implements RepositoryAbstract {
 			if (pos > 0) {
 				name = name.substring(0, pos);
 			}
-
-
-			//input = new FileInputStream("Repositorios/" + name + ".properties");
-			input = new FileInputStream("/home/rgarcia/storage/workspace/Tese-Repositorio/museumvictoria/src/main/java/museumvictoria.properties");
+			input = new FileInputStream("Repositorios/" + name + ".properties");
+			//input = new FileInputStream("/home/rgarcia/storage/workspace/Tese-Repositorio/MuseumVictoria/src/main/java/museumvictoria.properties");
 			prop.load(input);
 
-
 			repName = prop.getOrDefault("Name", "").toString();
-
 		}
 
 		return repName;
-
 	}
 
 }
---
+++ MuseumVictoria/src/main/java/repository/models/MuseumVictoriaRecordTypeModel.java
@@ -0,0 +1,9 @@
+package repository.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class MuseumVictoriaRecordTypeModel {
+    public static final String ARTICLE_RECORD_TYPE = "ARTICLE";
+    public static final String ITEM_RECORD_TYPE = "ITEM";
+    public static final String SPECIMEN_RECORD_TYPE = "SPECIMEN";
+}
